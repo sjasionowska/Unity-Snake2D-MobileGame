67fca1754d3fdf48976134382695fe9d782cbbad
sjasionowska <sjasionowska@gmail.com>
2018-04-15 18:37:05 +0200

    Buttons methods transferred to one script  added to ButtonsMethods object

---
+++ Snake/Assets/scripts/Buttons.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Buttons : MonoBehaviour {
+
+    public void OnClick(string name)
+    {
+        SceneManager.LoadScene(name);
+    }
+    
+}
